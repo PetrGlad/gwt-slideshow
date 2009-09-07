47feadc342972efcb341587de9959da8586a5e5b
Petr Gladkikh <PetrGlad@gmail.com>
2009-09-08 00:08:06 +0700

    Initial single image version.

--- src/greenlight/client/GreenLight.java
+++ src/greenlight/client/GreenLight.java
@@ -7,29 +7,24 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.event.dom.client.LoadEvent;
+import com.google.gwt.event.dom.client.LoadHandler;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GreenLight implements EntryPoint {
 	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
 	 */
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
@@ -38,57 +33,40 @@ public class GreenLight implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		Window.enableScrolling(false);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
+		final Button goButton = new Button("Go");
+		goButton.addStyleName("goButton");
+
+		final TextBox urlField = new TextBox();
+		urlField
+				.setText("http://farm3.static.flickr.com/2672/3858770113_831a33c36c_o.jpg");
+		urlField.setStyleName("urlField");
+
+		RootPanel.get("urlBar").add(urlField);
+		RootPanel.get("urlBar").add(goButton);
+
+		final Image image = new Image();
+		image.setStyleName("slideImage");
+		image.setVisible(false);
+		RootPanel.get("content").add(image);
+
+		final Label dimsLabel = new Label();
+
+		RootPanel.get("urlBar").add(dimsLabel);
 
 		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
+		urlField.setFocus(true);
+		urlField.selectAll();
 
 		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
+		class GoHandler implements ClickHandler, KeyUpHandler {
 			/**
 			 * Fired when the user clicks on the sendButton.
 			 */
 			public void onClick(ClickEvent event) {
-				sendNameToServer();
+				showShow();
 			}
 
 			/**
@@ -96,46 +74,59 @@ public class GreenLight implements EntryPoint {
 			 */
 			public void onKeyUp(KeyUpEvent event) {
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
+					showShow();
 				}
 			}
 
 			/**
-			 * Send the name from the nameField to the server and wait for a response.
+			 * Send the name from the nameField to the server and wait for a
+			 * response.
 			 */
-			private void sendNameToServer() {
-				sendButton.setEnabled(false);
-				String textToServer = nameField.getText();
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
+			private void showShow() {
+				image.setUrl(urlField.getText());
+				image.setVisible(true);
+				image.addLoadHandler(new LoadHandler() {
+					@Override
+					public void onLoad(LoadEvent event) {
+						fixupImageSizes(image, dimsLabel);						
+					}
+				});
+				dimsLabel.setText("Loading...");
 			}
 		}
 
+		Window.addResizeHandler(new ResizeHandler() {
+			@Override
+			public void onResize(ResizeEvent event) {
+				fixupImageSizes(image, dimsLabel);
+			}
+		});
+
 		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		GoHandler handler = new GoHandler();
+		goButton.addClickHandler(handler);
+		urlField.addKeyUpHandler(handler);
+	}
+
+	private void fixupImageSizes(Image image, Label dimsLabel) {
+		dimsLabel.setText("al=" + image.getAbsoluteLeft() + ",at="
+				+ image.getAbsoluteTop() + ",w=" + image.getWidth() + ",h="
+				+ image.getHeight() + ",oh=" + image.getOffsetHeight() + ",ow="
+				+ image.getOffsetWidth() + ",orl=" + image.getOriginLeft()
+				+ ",ort=" + image.getOriginTop() + ",ww="
+				+ Window.getClientWidth() + ",wh=" + Window.getClientHeight());
+		
+		// TODO: retain original aspect ratio
+		// XXX hardcode: need to get image padding somehow
+		final int padding = 3;
+		image
+				.setHeight(""
+						+ (Window.getClientHeight() - image.getAbsoluteTop() - padding * 2)
+						+ "px");
+		// XXX formatting: perhaps style's max-width should be sufficient
+		image
+				.setWidth(""
+						+ (Window.getClientWidth() - image.getAbsoluteLeft() - padding * 2)
+						+ "px");
 	}
 }
